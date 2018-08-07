687ffba0f7e28e4a9f0fec9cd7fc37e58ce54677
Hukanzen <qazxswed1934@gmail.com>
2018-08-07 23:13:41 +0900

    ツェラーの公式，zeller関数を追加 年月日から，曜日を求める．

--- main.rb
+++ main.rb
@@ -17,10 +17,40 @@ def power(x,y)
 	z
 end
 
+def zeller(q,m,k,j)
+	if m==1 then
+		m=13
+	elsif m==2 then
+		m=14
+	end
+	
+	h=(q+((m+1)*26/10)+k+k/4+j/4-2*j)%7
+	
+	if h == 0 then
+		dow="土曜日"
+	elsif h==1 then
+		dow="日曜日"
+	elsif h==2 then
+		dow="月曜日"
+	elsif h==3 then
+		dow="火曜日"
+	elsif h==4 then
+		dow="水曜日"
+	elsif h==5 then
+		dow="木曜日"
+	elsif h==6 then
+		dow="金曜日"
+	end
+	dow
+end
+
 
 z=factorial(6)
 print "#{z}"
 
 
 z=power(2,3)
+print "#{z}"
+
+z=zeller(8,8,18,20)
 print "#{z}"
